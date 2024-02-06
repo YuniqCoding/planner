845da8e334d61af0258561cb9fadc08ccd40b34b
YuniqCoding <rkdldk37@gmail.com>
2024-02-06 15:02:16 +0900

    응답에 StatusCode 및 Msg 추가

--- src/main/java/com/yuco/todoapp/controller/TodoController.java
+++ src/main/java/com/yuco/todoapp/controller/TodoController.java
@@ -1,8 +1,10 @@
 package com.yuco.todoapp.controller;
 
+import com.yuco.todoapp.CommonResponse;
 import com.yuco.todoapp.repository.ToDo;
 import com.yuco.todoapp.service.ToDoService;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,39 +19,63 @@ public class TodoController {
 
     public final ToDoService toDoService;
     @PostMapping
-    public ResponseEntity<ToDoResponseDto> postTodo(@RequestBody ToDoRequestDto dto){
+    public ResponseEntity<CommonResponse<ToDoResponseDto>> postTodo(@RequestBody ToDoRequestDto dto){
         //TODO 일정 작성 기능
         ToDo todo = toDoService.createToDo(dto);
         ToDoResponseDto response = new ToDoResponseDto(todo);
-        return ResponseEntity.ok().body(response);
+        return ResponseEntity.ok()
+                .body(CommonResponse.<ToDoResponseDto>builder()
+                        .statusCode(HttpStatus.OK.value())
+                        .msg("생성이 완료되었습니다.")
+                        .data(response)
+                        .build());
     }
 
     @GetMapping("/{todoId}")
-    public ResponseEntity<ToDoResponseDto> getTodo(@PathVariable Long todoId){
+    public ResponseEntity<CommonResponse<ToDoResponseDto>> getTodo(@PathVariable Long todoId) {
         ToDo todo = toDoService.getToDo(todoId);
         ToDoResponseDto response = new ToDoResponseDto(todo);
-        return ResponseEntity.ok().body(response);
+        return ResponseEntity.ok()
+                .body(CommonResponse.<ToDoResponseDto>builder()
+                        .statusCode(HttpStatus.OK.value())
+                        .msg("단건 조회가 완료 되었습니다.")
+                        .data(response)
+                        .build());
     }
+
     @GetMapping
-    public ResponseEntity<List<ToDoResponseDto>> getTodos(){
-        List<ToDo> todo = toDoService.getToDos();
-        List<ToDoResponseDto> response = todo.stream()
+    public ResponseEntity<CommonResponse<List<ToDoResponseDto>>> getTodos() {
+        List<ToDo> todos = toDoService.getToDos();
+        List<ToDoResponseDto> response = todos.stream()
                 .map(ToDoResponseDto::new)
                 .collect(Collectors.toList());
-        return ResponseEntity.ok().body(response);
+        return ResponseEntity.ok()
+                .body(CommonResponse.<List<ToDoResponseDto>>builder()
+                        .statusCode(HttpStatus.OK.value())
+                        .msg("목록 조회이 완료 되었습니다.")
+                        .data(response)
+                        .build());
     }
 
     @PutMapping("/{todoId}")
-    public ResponseEntity<ToDoResponseDto> putTodo(@PathVariable Long todoId,@RequestBody ToDoRequestDto dto){
-        ToDo todo = toDoService.updateToDo(todoId,dto);
+    public ResponseEntity<CommonResponse<ToDoResponseDto>> putTodo(@PathVariable Long todoId, @RequestBody ToDoRequestDto dto) {
+        ToDo todo = toDoService.updateToDo(todoId, dto);
         ToDoResponseDto response = new ToDoResponseDto(todo);
-        return ResponseEntity.ok().body(response);
+        return ResponseEntity.ok()
+                .body(CommonResponse.<ToDoResponseDto>builder()
+                        .statusCode(HttpStatus.OK.value())
+                        .msg("수정이 완료 되었습니다.")
+                        .data(response)
+                        .build());
     }
 
     @DeleteMapping("/{todoId}")
-    public ResponseEntity<Void> deleteTodo(@PathVariable Long todoId, @RequestBody ToDoRequestDto dto){
+    public ResponseEntity<CommonResponse> deleteTodo(@PathVariable Long todoId, @RequestBody ToDoRequestDto dto) {
         toDoService.deleteToDo(todoId, dto.getPassword());
-        return ResponseEntity.ok().build();
+        return ResponseEntity.ok().body(CommonResponse.builder()
+                .statusCode(HttpStatus.OK.value())
+                .msg("삭제가 완료 되었습니다.")
+                .build());
     }
 
 }
